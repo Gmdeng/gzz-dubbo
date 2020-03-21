52910d5f56dd03633dfa2c3df889b606d47ad409
Gm-X1 <hi.gmdeng@gmail.com>
2020-03-21 17:35:18 +0800

    init files

--- gzz-member-service/src/main/java/com/gzz/dubbo/member/service/dubboapi/MemberApiService.java
+++ gzz-member-service/src/main/java/com/gzz/dubbo/member/service/dubboapi/MemberApiService.java
@@ -1,9 +1,22 @@
 package com.gzz.dubbo.member.service.dubboapi;
 
+import com.gzz.dubbo.member.domain.Member;
+import com.gzz.dubbo.member.service.IMemberService;
 import org.apache.dubbo.config.annotation.Service;
 
+import java.util.List;
+
 @Service
-public class MemberApiService {
+public class MemberApiService implements IMemberService {
 
 
+    @Override
+    public Member getMember(String no) {
+        return null;
+    }
+
+    @Override
+    public List<Member> getMemberList() {
+        return null;
+    }
 }
